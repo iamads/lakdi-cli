dbb812de36442ef613dc11c6c963fb6047c28865
ads <abhijeet3192@gmail.com>
2016-01-26 01:40:33 +0530

    Fixed countermove bug

--- bot.py
+++ bot.py
@@ -49,19 +49,25 @@ class Bot(object):
 
     def makecountermove(self, currentcards):
         # cards belonging to same suit as of current round
-        suitofround = currentcards[0][2]
-        validcards = [self.prioritytable(i[2]) for i in self.mycards if i[
-                                    0] == suitofround]  # validcards I have for this round
+        suitofround = currentcards[0][0]
+        validcards = [i for i in self.mycards if i[0] == suitofround]  # validcards I have for this round
         # has anyone thrown trump in this round
         istrumpthere = [self.prioritytable(i[2])
                         for i in currentcards if i[0] == self.trump]
         istrumpthere.append(0)
+        print "validcards",len(validcards)
+        print "istrumpthere",len(istrumpthere)
         if len(validcards) > 0 and len(istrumpthere) == 0:
+            #print "daphne"
             return validcards.pop()
         elif len(validcards) > 0 and len(istrumpthere) > 0:
+            #print "velma"
             return validcards.pop(0)
         else:
+            #print "fred"
             if self.prioritytable(self.mytrumps[-1]) > max(istrumpthere):
+                #print "shaggy"
                 return self.mycards.pop(self.mytrumps.pop())
             else:
-                return self.mycards.pop()
+                #print "scooby"
+                return self.mycards.pop(validcards.pop())
